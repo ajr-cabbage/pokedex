e9481094cc0e8d127e37342ecd1613179a1bdfc6
ajr-cabbage <alexander.rakotz@gmail.com>
2026-05-05 23:53:02 -0400

    basic repl

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-        "fmt"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
-        fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		var input string
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			input = scanner.Text()
+		}
+		inputWords := cleanInput(input)
+		validCommands := getCommands()
+		cmd, ok := validCommands[inputWords[0]]
+		if ok {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Errorf("Error: %w", err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
+
 }
--- repl.go
+++ repl.go
@@ -1,11 +1,52 @@
 package main
 
 import (
-    "strings"
+	"fmt"
+	"os"
+	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
+	commands["help"] = cliCommand{
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	}
+	return commands
+}
+
+// returns lowered and stripped array of commands
 func cleanInput(text string) []string {
-    cleanedSlice := strings.Fields(strings.ToLower(text))
-    
-    return cleanedSlice
+	cleanedSlice := strings.Fields(strings.ToLower(text))
+	return cleanedSlice
+}
+
+// exit the program
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+// display the help message
+func commandHelp() error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
 }
